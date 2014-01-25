10398a4ebb80447c1155769bf1906dbb30cfa6e5
Tobias Heinemann <tobias.heinemann@gmail.com>
2014-01-24 17:27:36 -0800

    Added script that modifies various OS X settings

---
+++ defaults.sh
@@ -0,0 +1,3 @@
+# Disable auto rotate and scale when printing with Preview.app
+defaults write com.apple.Preview PVImagePrintingScaleMode 0
+defaults write com.apple.Preview PVImagePrintingAutoRotate 0
