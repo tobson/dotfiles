a7074eef8f567df913479b3e56413b7cb2f7f4e6
Tobias Heinemann <tobias.heinemann@gmail.com>
2014-05-14 12:22:40 -0700

    Set some more OS X defaults

--- defaults.sh
+++ defaults.sh
@@ -1,3 +1,12 @@
 # Disable auto rotate and scale when printing with Preview.app
 defaults write com.apple.Preview PVImagePrintingScaleMode 0
 defaults write com.apple.Preview PVImagePrintingAutoRotate 0
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
